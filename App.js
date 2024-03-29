405be141f9afa4864cc9709562f63fa7c26197dc
Oleksandr Bezditnyi <oleksandrbezditny@gmail.com>
2024-03-29 08:02:04 +0100

    todo. scroll content

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import {StatusBar} from 'expo-status-bar';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useState} from "react";
 
 export default function App() {
@@ -24,11 +24,13 @@ export default function App() {
                 <Button title='Add Goal' onPress={addGoalHandler}/>
             </View>
             <View style={styles.goalsContainer}>
-                {courseGoals.map((goal, index) => (
-                    <View key={index} style={styles.goalItem}>
-                        <Text style={styles.goalText}>{goal}</Text>
-                    </View>
-                ))}
+                <ScrollView>
+                    {courseGoals.map((goal, index) => (
+                        <View key={index} style={styles.goalItem}>
+                            <Text style={styles.goalText}>{goal}</Text>
+                        </View>
+                    ))}
+                </ScrollView>
             </View>
         </View>
     );
