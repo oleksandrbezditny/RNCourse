ea44da511b0c83da308fb0cd0aee451479636ebf
Oleksandr Bezditnyi <oleksandrbezditny@gmail.com>
2024-04-01 19:01:08 +0200

    finish styling

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {Button, FlatList, Pressable, StyleSheet, View} from 'react-native';
 import {useState} from "react";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
+import {StatusBar} from "expo-status-bar";
 
 export default function App() {
     const [courseGoals, setCourseGoals] = useState([]);
@@ -31,23 +32,28 @@ export default function App() {
     }
 
     return (
-        <View style={styles.appContainer}>
-            <Button title='Add new goal' color="#5e0acc" onPress={startAddGoalHandler} />
-            <GoalInput
-                onAddGoal={addGoalHandler}
-                visible={modalIsVisible}
-                onCancel={endAddGoalHandler}
-            />
-            <View style={styles.goalsContainer}>
-                <FlatList data={courseGoals} renderItem={(itemData) => <GoalItem text={itemData.item.text} id={itemData.item.key} onDeleteItem={deleteGoalHandler} />}/>
+        <>
+            <StatusBar style={'light'} />
+            <View style={styles.appContainer}>
+                <Button title='Add new goal' color="#a065ec" onPress={startAddGoalHandler} />
+                <GoalInput
+                    onAddGoal={addGoalHandler}
+                    visible={modalIsVisible}
+                    onCancel={endAddGoalHandler}
+                />
+                <View style={styles.goalsContainer}>
+                    <FlatList data={courseGoals} renderItem={(itemData) => <GoalItem text={itemData.item.text} id={itemData.item.key} onDeleteItem={deleteGoalHandler} />}/>
+                </View>
             </View>
-        </View>
+        </>
     );
 }
 
 const styles = StyleSheet.create({
     appContainer: {
-        paddingTop: 50, paddingHorizontal: 16, flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16,
+        flex: 1,
     }, goalsContainer: {
         flex: 5,
     }
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,25 +1,30 @@
-import {Button, Modal, StyleSheet, TextInput, View} from "react-native";
+import {Button, Image, Modal, StyleSheet, TextInput, View} from "react-native";
 import {useState} from "react";
 
 const styles = StyleSheet.create({
     textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
         width: '100%',
-        padding: 8
+        padding: 16
     }, inputContainer: {
         flex: 1,
-        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 22,
-        borderBottomWidth: 1,
-        borderBottomColor: '#CCCCCC',
         padding: 16,
+        backgroundColor: '#311b6b'
     }, buttonContainer: {
         flexDirection: "row", marginTop: 16,
     }, button: {
         width: 100, marginHorizontal: 8
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20
     }
 });
 
@@ -37,11 +42,12 @@ function GoalInput({onAddGoal, visible, onCancel}) {
 
     return (<Modal visible={visible} animationType="slide">
             <View style={styles.inputContainer}>
+                <Image style={styles.image} source={require('../assets/images/goal.png')} />
                 <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={goalInputHandler}
                            value={enteredGoalText}/>
                 <View style={styles.buttonContainer}>
-                    <View style={styles.button}><Button title='Add Goal' onPress={addGoalHandler}/></View>
-                    <View style={styles.button}><Button title='Cancel' onPress={onCancel}/></View>
+                    <View style={styles.button}><Button title='Cancel' onPress={onCancel} color="#f31282" /></View>
+                    <View style={styles.button}><Button title='Add Goal' onPress={addGoalHandler} color="#5e0acc"/></View>
                 </View>
             </View>
         </Modal>);
